7bb47df3eda72a86f6d6ff3745e9c77777f1d3ef
Riccardo Musmeci <riccardomusmeci92@gmail.com>
2024-03-05 21:14:28 +0100

    train script

---
+++ train.py
@@ -0,0 +1,83 @@
+import argparse
+import json
+import os
+import time
+from functools import partial
+from shutil import copy2
+
+import mlx.core as mx
+import mlx.nn as nn
+import mlx.optimizers as optim
+
+from mlxim.callbacks import ModelCheckpoint
+from mlxim.data import DataLoader, LabelFolderDataset
+from mlxim.io.config import load_config
+from mlxim.metrics.classification import Accuracy
+from mlxim.model import create_model
+from mlxim.trainer.trainer import Trainer
+from mlxim.transform import ImageNetTransform
+from mlxim.utils.time import now
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Training script")
+    parser.add_argument("--config", type=str, default="config/train.yaml")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    config = load_config(args.config)
+    mx.random.seed(config["seed"])
+
+    # creating output dir + copying config
+    output_dir = os.path.join(config["output"], now())
+    os.makedirs(output_dir, exist_ok=True)
+    copy2(args.config, output_dir)
+
+    # define datasets
+    train_dataset = LabelFolderDataset(
+        root_dir=os.path.join(config["data_dir"], "train"),
+        transform=ImageNetTransform(train=True, **config["transform"]),
+        **config["dataset"],
+    )
+
+    val_dataset = LabelFolderDataset(
+        root_dir=os.path.join(config["data_dir"], "val"),
+        transform=ImageNetTransform(train=False, **config["transform"]),
+        **config["dataset"],
+    )
+
+    # define loader + optimizer
+    model = create_model(
+        num_classes=len(train_dataset.class_map),
+        **config["model"],
+    )
+    optimizer = optim.Adam(learning_rate=1e-3)
+
+    # define loaders
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        **config["loader"],
+    )
+
+    config["loader"]["shuffle"] = False
+    val_loader = DataLoader(dataset=val_dataset, **config["loader"])
+
+    # define model checkpoint
+    model_checkpoint = ModelCheckpoint(output_dir=os.path.join(config["output"], now()), **config["model_checkpoint"])
+
+    # trainer
+    trainer = Trainer(
+        model=model,
+        optimizer=optimizer,
+        loss_fn=nn.losses.cross_entropy,
+        loss_fn_args={"label_smoothing": 0.0},
+        train_loader=train_loader,
+        val_loader=val_loader,
+        log_every=0.05,
+        model_checkpoint=model_checkpoint,
+    )
+
+    trainer.train()
