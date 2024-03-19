1fd70fe7d5e24893182f7ed4bc4a45e32ae3b92b
Riccardo Musmeci <riccardomusmeci92@gmail.com>
2024-03-19 21:46:20 +0100

    lighter to read registry

--- src/mlxim/model/_registry.py
+++ src/mlxim/model/_registry.py
@@ -1,40 +1,15 @@
-from .resnet import *
-from .vit import *
+from .resnet import RESNET_CONFIG, RESNET_ENTRYPOINT
+from .swin_transformer import SWIN_CONFIG, SWIN_ENTRYPOINT
+from .vit import VIT_CONFIG, VIT_ENTRYPOINT
 
-MODEL_ENTRYPOINT = {
-    "resnet18": resnet18,
-    "resnet34": resnet34,
-    "resnet50": resnet50,
-    "resnet101": resnet101,
-    "resnet152": resnet152,
-    "wide_resnet50_2": wide_resnet50_2,
-    "wide_resnet101_2": wide_resnet101_2,
-    "vit_base_patch16_224": vit_base_patch16_224,
-    "vit_base_patch16_224.swag_lin": vit_base_patch16_224,
-    "vit_base_patch32_224": vit_base_patch32_224,
-    "vit_base_patch16_384.swag_e2e": vit_base_patch16_384,
-    "vit_large_patch16_224": vit_large_patch16_224,
-    "vit_large_patch16_224.swag_lin": vit_large_patch16_224,
-    "vit_large_patch16_512.swag_e2e": vit_large_patch16_512,
-    "vit_huge_patch14_224.swag_lin": vit_huge_patch14_224,
-    "vit_huge_patch14_518.swag_e2e": vit_huge_patch14_518,
-}
+###### Registry ######
+MODEL_ENTRYPOINT = {}
+MODEL_ENTRYPOINT.update(RESNET_ENTRYPOINT)
+MODEL_ENTRYPOINT.update(VIT_ENTRYPOINT)  # type: ignore
+MODEL_ENTRYPOINT.update(SWIN_ENTRYPOINT)  # type: ignore
 
-MODEL_CONFIG = {
-    "resnet18": resnet_configs["resnet18"],
-    "resnet34": resnet_configs["resnet34"],
-    "resnet50": resnet_configs["resnet50"],
-    "resnet101": resnet_configs["resnet101"],
-    "resnet152": resnet_configs["resnet152"],
-    "wide_resnet50_2": resnet_configs["wide_resnet50_2"],
-    "wide_resnet101_2": resnet_configs["wide_resnet101_2"],
-    "vit_base_patch16_224": vit_configs["vit_base_patch16_224"],
-    "vit_base_patch16_224.swag_lin": vit_configs["vit_base_patch16_224.swag_lin"],
-    "vit_base_patch32_224": vit_configs["vit_base_patch32_224"],
-    "vit_base_patch16_384.swag_e2e": vit_configs["vit_base_patch16_384.swag_e2e"],
-    "vit_large_patch16_224": vit_configs["vit_large_patch16_224"],
-    "vit_large_patch16_224.swag_lin": vit_configs["vit_large_patch16_224.swag_lin"],
-    "vit_large_patch16_512.swag_e2e": vit_configs["vit_large_patch16_512.swag_e2e"],
-    "vit_huge_patch14_224.swag_lin": vit_configs["vit_huge_patch14_224.swag_lin"],
-    "vit_huge_patch14_518.swag_e2e": vit_configs["vit_huge_patch14_518.swag_e2e"],
-}
+###### Config ######
+MODEL_CONFIG = {}
+MODEL_CONFIG.update(RESNET_CONFIG)
+MODEL_CONFIG.update(VIT_CONFIG)
+MODEL_CONFIG.update(SWIN_CONFIG)
