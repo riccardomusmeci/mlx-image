e87c3895b9de9d7cb3b98458170bbaffde68b80f
Riccardo Musmeci <riccardomusmeci92@gmail.com>
2024-03-20 08:59:08 +0100

    fix swinv2 name

--- src/mlxim/model/swin_transformer/__init__.py
+++ src/mlxim/model/swin_transformer/__init__.py
@@ -3,9 +3,9 @@ from ._factory import (
     swin_configs,
     swin_small_patch4_window7_224,
     swin_tiny_patch4_window7_224,
-    swin_v2_base_patch4_window8_224,
-    swin_v2_small_patch4_window8_224,
-    swin_v2_tiny_patch4_window8_224,
+    swin_v2_base_patch4_window8_256,
+    swin_v2_small_patch4_window8_256,
+    swin_v2_tiny_patch4_window8_256,
 )
 
 __all__ = [
@@ -17,16 +17,16 @@ SWIN_ENTRYPOINT = {
     "swin_tiny_patch4_window7_224": swin_tiny_patch4_window7_224,
     "swin_small_patch4_window7_224": swin_small_patch4_window7_224,
     "swin_base_patch4_window7_224": swin_base_patch4_window7_224,
-    "swin_v2_tiny_patch4_window8_224": swin_v2_tiny_patch4_window8_224,
-    "swin_v2_small_patch4_window8_224": swin_v2_small_patch4_window8_224,
-    "swin_v2_base_patch4_window8_224": swin_v2_base_patch4_window8_224,
+    "swin_v2_tiny_patch4_window8_256": swin_v2_tiny_patch4_window8_256,
+    "swin_v2_small_patch4_window8_256": swin_v2_small_patch4_window8_256,
+    "swin_v2_base_patch4_window8_256": swin_v2_base_patch4_window8_256,
 }
 
 SWIN_CONFIG = {
     "swin_tiny_patch4_window7_224": swin_configs["swin_tiny_patch4_window7_224"],
     "swin_small_patch4_window7_224": swin_configs["swin_small_patch4_window7_224"],
     "swin_base_patch4_window7_224": swin_configs["swin_base_patch4_window7_224"],
-    "swin_v2_tiny_patch4_window8_224": swin_configs["swin_v2_tiny_patch4_window8_224"],
-    "swin_v2_small_patch4_window8_224": swin_configs["swin_v2_small_patch4_window8_224"],
-    "swin_v2_base_patch4_window8_224": swin_configs["swin_v2_base_patch4_window8_224"],
+    "swin_v2_tiny_patch4_window8_256": swin_configs["swin_v2_tiny_patch4_window8_256"],
+    "swin_v2_small_patch4_window8_256": swin_configs["swin_v2_small_patch4_window8_256"],
+    "swin_v2_base_patch4_window8_256": swin_configs["swin_v2_base_patch4_window8_256"],
 }
--- src/mlxim/model/swin_transformer/_factory.py
+++ src/mlxim/model/swin_transformer/_factory.py
@@ -22,20 +22,20 @@ swin_configs = {
         transform=Transform(img_size=224, crop_pct=224 / 238, interpolation="bicubic"),
         weights=HFWeights(repo_id="mlx-vision/swin_base_patch4_window7_224-mlxim", filename="model.safetensors"),
     ),
-    "swin_v2_tiny_patch4_window8_224": ModelConfig(
+    "swin_v2_tiny_patch4_window8_256": ModelConfig(
         metrics=Metrics(dataset="ImageNet-1K", accuracy_at_1=0.80806, accuracy_at_5=0.95178),
         transform=Transform(img_size=256, crop_pct=256 / 260, interpolation="bicubic"),
-        weights=HFWeights(repo_id="mlx-vision/swin_v2_tiny_patch4_window8_224-mlxim", filename="model.safetensors"),
+        weights=HFWeights(repo_id="mlx-vision/swin_v2_tiny_patch4_window8_256-mlxim", filename="model.safetensors"),
     ),
-    "swin_v2_small_patch4_window8_224": ModelConfig(
+    "swin_v2_small_patch4_window8_256": ModelConfig(
         metrics=Metrics(dataset="ImageNet-1K", accuracy_at_1=0.81614, accuracy_at_5=0.95515),
         transform=Transform(img_size=256, crop_pct=256 / 260, interpolation="bicubic"),
-        weights=HFWeights(repo_id="mlx-vision/swin_s2_small_patch4_window8_224-mlxim", filename="model.safetensors"),
+        weights=HFWeights(repo_id="mlx-vision/swin_s2_small_patch4_window8_256-mlxim", filename="model.safetensors"),
     ),
-    "swin_v2_base_patch4_window8_224": ModelConfig(
+    "swin_v2_base_patch4_window8_256": ModelConfig(
         metrics=Metrics(dataset="ImageNet-1K", accuracy_at_1=0.81752, accuracy_at_5=0.95655),
         transform=Transform(img_size=256, crop_pct=256 / 272, interpolation="bicubic"),
-        weights=HFWeights(repo_id="mlx-vision/swin_v2_base_patch4_window8_224-mlxim", filename="model.safetensors"),
+        weights=HFWeights(repo_id="mlx-vision/swin_v2_base_patch4_window8_256-mlxim", filename="model.safetensors"),
     ),
 }
 
@@ -75,7 +75,7 @@ def swin_base_patch4_window7_224(num_classes: int = 1000) -> SwinTransformer:
     )
 
 
-def swin_v2_tiny_patch4_window8_224(num_classes: int = 1000) -> SwinTransformer:
+def swin_v2_tiny_patch4_window8_256(num_classes: int = 1000) -> SwinTransformer:
     return SwinTransformer(
         patch_size=[4, 4],
         embed_dim=96,
@@ -89,7 +89,7 @@ def swin_v2_tiny_patch4_window8_224(num_classes: int = 1000) -> SwinTransformer:
     )
 
 
-def swin_v2_base_patch4_window8_224(num_classes: int = 1000) -> SwinTransformer:
+def swin_v2_base_patch4_window8_256(num_classes: int = 1000) -> SwinTransformer:
     return SwinTransformer(
         patch_size=[4, 4],
         embed_dim=128,
@@ -103,7 +103,7 @@ def swin_v2_base_patch4_window8_224(num_classes: int = 1000) -> SwinTransformer:
     )
 
 
-def swin_v2_small_patch4_window8_224(num_classes: int = 1000) -> SwinTransformer:
+def swin_v2_small_patch4_window8_256(num_classes: int = 1000) -> SwinTransformer:
     return SwinTransformer(
         patch_size=[4, 4],
         embed_dim=96,
@@ -115,62 +115,3 @@ def swin_v2_small_patch4_window8_224(num_classes: int = 1000) -> SwinTransformer
         downsample_layer=PatchMergingV2,
         num_classes=num_classes,
     )
-
-
-# def swin_v2_t(pretrained: bool = True, **kwargs) -> SwinTransformer:
-#     model = _swin_transformer(
-#         patch_size=[4, 4],
-#         embed_dim=96,
-#         depths=[2, 2, 6, 2],
-#         num_heads=[3, 6, 12, 24],
-#         window_size=[8, 8],
-#         stochastic_depth_prob=0.2,
-#         block=SwinTransformerBlockV2,
-#         downsample_layer=PatchMergingV2,
-#         **kwargs,
-#     )
-#     if pretrained:
-#         weights_url = "https://download.pytorch.org/models/swin_v2_t-b137f0e2.pth"
-#         weights = get_pytorch_weights(weights_url)
-#         model.load_pytorch_weights(weights)
-#     return model
-
-
-# @register_model()
-# def swin_v2_s(pretrained: bool = True, **kwargs) -> SwinTransformer:
-#     model = _swin_transformer(
-#         patch_size=[4, 4],
-#         embed_dim=96,
-#         depths=[2, 2, 18, 2],
-#         num_heads=[3, 6, 12, 24],
-#         window_size=[8, 8],
-#         stochastic_depth_prob=0.3,
-#         block=SwinTransformerBlockV2,
-#         downsample_layer=PatchMergingV2,
-#         **kwargs,
-#     )
-#     if pretrained:
-#         weights_url = "https://download.pytorch.org/models/swin_v2_s-637d8ceb.pth"
-#         weights = get_pytorch_weights(weights_url)
-#         model.load_pytorch_weights(weights)
-#     return model
-
-
-# @register_model()
-# def swin_v2_b(pretrained: bool = True, **kwargs) -> SwinTransformer:
-#     model = _swin_transformer(
-#         patch_size=[4, 4],
-#         embed_dim=128,
-#         depths=[2, 2, 18, 2],
-#         num_heads=[4, 8, 16, 32],
-#         window_size=[8, 8],
-#         stochastic_depth_prob=0.5,
-#         block=SwinTransformerBlockV2,
-#         downsample_layer=PatchMergingV2,
-#         **kwargs,
-#     )
-#     if pretrained:
-#         weights_url = "https://download.pytorch.org/models/swin_v2_b-781e5279.pth"
-#         weights = get_pytorch_weights(weights_url)
-#         model.load_pytorch_weights(weights)
-#     return model
